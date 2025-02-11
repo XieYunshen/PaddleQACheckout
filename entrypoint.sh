da8b42eb186cd715e2abeb35feece012bd5043c9
XieYunshen <1084314248@qq.com>
2025-02-11 17:32:10 +0800

    优化代码clone逻辑,支持PR代码下载

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ echo "time=$time" >> $GITHUB_OUTPUT
 git_version=$(git --version)
 echo "git_version=$git_version" >> $GITHUB_OUTPUT
 
-# 初始化函数，简化逻辑，增加默认值处理
+# Initialize function to simplify logic and handle default values
 function init() {
-    # 默认值设置
+    # Handling default values
     CLONE_REPO="https://github.com/${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}"
-    CLONE_BRANCH="${INPUT_REF:+-b ${INPUT_REF:-main}}"
+    CLONE_REF="${INPUT_REF:+-b ${INPUT_REF:-main}}"
     if [[ -z "${INPUT_PATH}" ]];then
         if [[ -z "${INPUT_REPOSITORY}" ]];then
             CLONE_PATH="${GITHUB_REPOSITORY##*/}"
@@ -21,17 +21,22 @@ function init() {
         CLONE_PATH="${INPUT_PATH}"
     fi
     CLONE_DEPTH="${INPUT_FETCH_DEPTH:+--depth ${INPUT_FETCH_DEPTH:-1}}"
-    COMMIT_SHA=${INPUT_COMMIT_SHA}
+    if [[ -z "${INPUT_BRANCH_NAME}" ]];then
+        BRANCH_NAME=${CLONE_REF}
+    fi
+    # Repository compression setting
     REPO_COMPRESS=${INPUT_REPO_COMPRESS}
 
-    # 输出信息，确认设置
+    # Output information to confirm settings
     echo "Cloning repository: $CLONE_REPO"
-    echo "Clone branch: $CLONE_BRANCH"
+    echo "Clone ref: $CLONE_REF"
+    echo "Branch Name: $BRANCH_NAME"
     echo "Clone path: $CLONE_PATH"
     echo "Clone depth: $CLONE_DEPTH"
+    echo "Repo compress: $REPO_COMPRESS"
 }
 
-# 判断是否需要初始化子模块，逻辑保持不变
+# Check if submodule initialization is needed;
 function init_submodule() {
     if [[ "${INPUT_SUBMODULES}" == "true" ]];then
         git submodule update --init
@@ -42,38 +47,7 @@ function init_submodule() {
     fi
 }
 
-# 检出到指定commit_sha
-function checkout_commit_sha() {
-    if [[ "${COMMIT_SHA}" == "" ]];then
-        echo "commit_sha not provided."
-    else
-        # 检查 commit 是否存在
-        if git show "$COMMIT_SHA" &>/dev/null; then
-            echo "Commit exists"
-        else
-            echo "Commit does not exist. Attempting to fetch and pull updates..."
-            # 执行 git pull 和 git fetch --unshallow
-            git pull origin $(git rev-parse --abbrev-ref HEAD)  # 拉取当前分支的最新更新
-
-            # 如果仓库是浅克隆，拉取完整历史
-            if git rev-parse --is-shallow-repository &>/dev/null; then
-                echo "Repository is shallow, fetching full history..."
-                git fetch --unshallow
-            fi
-        fi
-        # 再次检查 commit 是否存在
-        if git show "$COMMIT_SHA" &>/dev/null; then
-            echo "Commit exists after fetching updates"
-        else
-            echo "Commit still does not exist after fetching updates"
-            exit 1  # 如果仍然不存在，退出并报错
-        fi
-        # 切换到指定的commit_sha
-        git reset --hard ${COMMIT_SHA}
-    fi
-}
-
-# 代码库清理和压缩
+# Repository cleanup and compression
 function clean_and_compress_repo() {
     if [[ "${INPUT_REPO_COMPRESS}" == "true" ]];then
         # Remove unreachable objects and pack loose objects
@@ -90,13 +64,24 @@ function clean_and_compress_repo() {
     fi
 }
 
-# 主函数，改进错误处理和命令执行
+# Main function
 function main() {
     init
-    echo "git clone $CLONE_REPO $CLONE_BRANCH $CLONE_DEPTH $CLONE_PATH"
     mkdir -p $CLONE_PATH && cd $CLONE_PATH
-    git clone $CLONE_REPO $CLONE_BRANCH $CLONE_DEPTH . || { echo "Git clone failed"; exit 1; }
-    checkout_commit_sha || { echo "Checkout commit sha failed"; exit 2; }
+    git init
+    git remote add origin $CLONE_REPO
+    git fetch $CLONE_DEPTH origin $CLONE_REF
+    git checkout --force $CLONE_REF
+    # Get the current branch name
+    branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+    # Check if it's in detached HEAD state
+    if [[ "$branch_name" == "HEAD" ]]; then
+        echo "You are in 'detached HEAD' state."
+        git checkout -b $BRANCH_NAME
+    else
+        echo "You are not in 'detached HEAD' state."
+    fi
     init_submodule || { echo "Submodule initialization failed"; exit 2; }
     clean_and_compress_repo
 }
